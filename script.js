0295d94c7cc558e45e6a27a14de4adb06073b933
Alberto-Dorian <babyfacebad@hotmail.com>
2026-08-01 02:40:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function compartir(){
 
+if(navigator.share){
+
+navigator.share({
+title:"Baila Sin Control",
+text:"Escucha el nuevo sencillo de Alberto Dorian",
+url:window.location.href
+});
+
+}else{
+
+navigator.clipboard.writeText(window.location.href);
+
+alert("¡Link copiado!");
+
+}
+
+}
